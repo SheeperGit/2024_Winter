6c25af7380aead449c89e63a68b44ab9f699c965
SheeperGit <126368846+SheeperGit@users.noreply.github.com>
2024-02-08 21:22:10 -0500

    C24Lab4 done

--- C24/L/4/lab4.rkt
+++ C24/L/4/lab4.rkt
@@ -53,22 +53,29 @@
 (check-expect (my-reverse '(42)) '(42))
 (check-expect (my-reverse '(1 2 3)) '(3 2 1))
 
-; What is the time complexity of my-reverse?
-; What is the space complexity of my-reverse?
+; What is the time complexity of my-reverse?    O(N)?
+; What is the space complexity of my-reverse?   O(N^2)?
 
 ; (reverse-tail xs) -> list?
 ; xs: list?
 ; returns the reverse of xs
 ; a tail recursive version
-(define (reverse-tail xs)        ; return reverse[xs]
-  42)
+(define (reverse-tail xs) ; return reverse[xs]
+  (define (reverse-helper xs acc)
+    (if (empty? xs)
+        acc
+        (reverse-helper (rest xs) (cons (first xs) acc))))
+  (reverse-helper xs '()))
+
+;; Explanation: We append the last elem of `xs` (using rest) to the list `acc`
+;;              When `xs` is empty, we return `acc`.
 
 (check-expect (reverse-tail '()) '())
 (check-expect (reverse-tail '(42)) '(42))
 (check-expect (reverse-tail '(1 2 3)) '(3 2 1))
 
-; What is the time complexity of reverse-tail?
-; What is the space complexity of reverse-tail?
+; What is the time complexity of reverse-tail?    O(N)?
+; What is the space complexity of reverse-tail?   O(N)?
 
 ; (num-els xs) -> integer?
 ; xs: list?
@@ -84,15 +91,36 @@
 
 ; using HOPs (and recursion)
 (define (num-els-hop xs)
-  42)
+  (define (num-els-hop-helper xs)
+    (if (empty? xs)
+        0
+        (if (list? (first xs))
+            (+ (num-els-hop-helper (first xs)) (num-els-hop-helper (rest xs)))
+            (+ 1 (num-els-hop-helper (rest xs))))))
+  (num-els-hop-helper xs))
+
+
 
 (check-expect (num-els-hop '(1 (2 (3 4 ((((5))) 6) 7)) 8)) 8)
+(check-expect (num-els-hop '(1 2 3)) 3)
+(check-expect (num-els-hop '()) 0)
+(check-expect (num-els-hop '(1)) 1)
 
 ; tail-recursive version
 (define (num-els-tail xs)
-  42)
+  (define (num-els-tail-helper xs acc)
+    (if (empty? xs)
+        acc
+        (if (list? (first xs))
+            (num-els-tail-helper (rest xs) (+ acc (num-els-tail (first xs))))
+            (num-els-tail-helper (rest xs) (+ acc 1)))))
+  (num-els-tail-helper xs 0))
+
 
 (check-expect (num-els-tail '(1 (2 (3 4 ((((5))) 6) 7)) 8)) 8)
+(check-expect (num-els-tail '(1 2 3)) 3)
+(check-expect (num-els-tail '()) 0)
+(check-expect (num-els-tail '(1)) 1)
 
 ; (sum-els xs) -> number?
 ; xs: list of number?
@@ -109,13 +137,25 @@
 
 ; using HOPs (and recursion)
 (define (sum-els-hop xs)
-  42)
+  (define (sum-els-hop-helper xs)
+    (if (empty? xs)
+        0
+        (if (list? (first xs))
+            (+ (sum-els-hop-helper (first xs)) (sum-els-hop (rest xs)))
+            (+ (first xs) (sum-els-hop-helper (rest xs))))))
+  (sum-els-hop-helper xs))
 
 (check-expect (sum-els-hop '(1 (2 (3 4 ((((5))) 6) 7)) 8)) 36)
 
 ; tail-recursive version
 (define (sum-els-tail xs)
-  42)
+  (define (sum-els-tail-helper xs acc)
+    (if (empty? xs)
+        acc
+        (if (list? (first xs))
+            (sum-els-tail-helper (rest xs) (+ acc (sum-els-tail (first xs))))
+            (sum-els-tail-helper (rest xs) (+ acc (first xs))))))
+  (sum-els-tail-helper xs 0))
 
 (check-expect (sum-els-tail '(1 (2 (3 4 ((((5))) 6) 7)) 8)) 36)
 
@@ -124,21 +164,41 @@
 ; Returns the flattened version of xs.
 ; Recursive. Not tail-recursive.
 (define (flatten xs)
- 42)
+  (cond
+    ((empty? xs) '())   ; Base Case 1: xs is empty (and so is flat.)
+    ((not (pair? xs)) (list xs))  ; Base Case 2: xs is not a list, and so xs is not nested.
+    (else (append (flatten (first xs)) (flatten (rest xs))))))  ; Recursive Step: Flatten the car and cdr of xs, and append the results.
+
 
 (check-expect (flatten '(1 (2 (3 4 ((((5))) 6) 7)) 8))
               '(1 2 3 4 5 6 7 8))
 
 ; using HOPs (and recursion)
 (define (flatten-hop xs)
-  42)
+  (define (flatten-hop-helper xs)
+    (cond
+      ((empty? xs) '())  ; Base Case 1: xs is empty (and so is flat.)
+      ((not (pair? xs)) (list xs))  ; ; Base Case 2: xs is not a list, and so xs is not nested.
+      (else (append-map flatten-hop xs))))  ; Recursive Step: Use `append-map` to flatten each elem of xs.
+  (flatten-hop-helper xs))
+;; Note: `append-map` applies a function to each elem of a list and appends the results.
+;;        Effectively, `map` then `append`. (Neat function!)
+
 
 (check-expect (flatten-hop '(1 (2 (3 4 ((((5))) 6) 7)) 8))
               '(1 2 3 4 5 6 7 8))
 
 ; tail-recursive version
 (define (flatten-tail xs)
-  42)
+  (define (flatten-tail-helper xs acc)
+    (cond
+      ((empty? xs) acc)   ; Base Case 1: xs is empty, return `acc`.
+      ((not (pair? xs)) (cons xs acc))    ; Base Case 2: xs is not a list, add xs to `acc`.
+      (else (flatten-tail-helper (rest xs)    ; Recursive Step: Found nested list. Process first elem (will be added to the accumulator eventually.)
+                            (flatten-tail-helper (first xs) acc))))) ; Flatten the first of xs, then add it `acc`.
+
+  (reverse (flatten-tail-helper xs '())))   ; Reverse `acc` since we're doing this process backwards.
+
 
 (check-expect (flatten-tail '(1 (2 (3 4 ((((5))) 6) 7)) 8))
               '(1 2 3 4 5 6 7 8))
