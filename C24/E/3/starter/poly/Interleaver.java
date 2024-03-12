6f5bf394f1bb217dc3db95e48013c45e976a45dc
SheeperGit <126368846+SheeperGit@users.noreply.github.com>
2024-03-11 21:24:05 -0400

    C24E3 Q3 done, E3 complete

--- C24/E/3/starter/poly/Interleaver.java
+++ C24/E/3/starter/poly/Interleaver.java
@@ -1,4 +1,8 @@
 package poly;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 /**
@@ -13,7 +17,15 @@ public class Interleaver {
    * This method should not use loops. Use Streams and Stream methods instead!
    *
    */
-    // interleave
+  // We'll cheat by making the return type a list of Objects, since every obj is a subclass of class Object :p //
+  // This ensures this function will work on any object type! I suppose Java *can* be cool sometimes :/        //
+  public static <X,Y> List<Object> interleave(List<X> xs, List<Y> ys) {
+    int minLen = Math.min(xs.size(), ys.size());
+      return IntStream.range(0, minLen)                       // Suppose xs = [1, 2, 3], ys = [4, 5, 6]
+        .mapToObj(i -> new Pair<>(xs.get(i), ys.get(i)))      // (1, 4), (2, 5), (3, 6)
+        .flatMap(pair -> Stream.of(pair.first, pair.second))  // (1, 4, 2, 5, 3, 6)
+        .collect(Collectors.toList());                        // [1, 4, 2, 5, 3, 6]
+  }
 
   /**
    * Returns a List of Pairs of corresponding elements from xs and ys,
@@ -22,5 +34,10 @@ public class Interleaver {
    * This method should not use loops. Use Streams and Stream methods instead!
    *
    */
-    // toPairs
+  public static <X,Y> List<Pair<X, Y>> toPairs(List<X> xs, List<Y> ys) {
+    int minLen = Math.min(xs.size(), ys.size());
+    return IntStream.range(0, minLen)                   // Suppose xs = [1, 2, 3], ys = [4, 5, 6]
+      .mapToObj(i -> new Pair<>(xs.get(i), ys.get(i)))  // (1, 4), (2, 5), (3, 6)
+      .collect(Collectors.toList());                    // [(1, 4), (2, 5), (3, 6)]
+  }
 }
--- C24/E/3/starter/poly/Pair.java
+++ C24/E/3/starter/poly/Pair.java
@@ -3,4 +3,26 @@ package poly;
 /**
  * A Pair of X and Y.
  */
-public class Pair {}
+public class Pair<X, Y> {
+    public final X first;
+    public final Y second;
+
+    public Pair(X first, Y second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /* Uncomment to debug */
+    // public X getFirst() {
+    //     return first;
+    // }
+
+    // public Y getSecond() {
+    //     return second;
+    // }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+}
