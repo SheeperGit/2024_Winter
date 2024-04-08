01e950d146b774bf6c2b5fe3b41c5df907290ae7
SheeperGit <126368846+SheeperGit@users.noreply.github.com>
2024-04-08 14:07:00 -0400

    C24E6 map.c style changes

--- C24/E/6/code/map.c
+++ C24/E/6/code/map.c
@@ -9,42 +9,40 @@ int mult(size_t i, int j) {
     return i * j;
 }
 
-
 void apply(int *arr, size_t len, int (*fnPtr)(int)) {
-     for(size_t i = 0; i < len; i++) {
-         arr[i] = fnPtr(arr[i]);
-     }             
-};
+    for(size_t i = 0; i < len; i++) {
+        arr[i] = fnPtr(arr[i]);
+    }             
+}
 
 void apply_enumerate(int *arr, size_t len, int (*fnPtr)(size_t, int)) {
-     for(size_t i = 0; i < len; i++) {
-         arr[i] = fnPtr(i, arr[i]);
-     }             
-};
-
+    for(size_t i = 0; i < len; i++) {
+        arr[i] = fnPtr(i, arr[i]);
+    }             
+}
 
 void print_array(int *arr, size_t len, char *sep) {
-     if(sep == NULL) {
-	sep = ", ";
-     }
+    if(sep == NULL) {
+        sep = ", ";
+    }
+    printf("%d", *arr);
 
-     printf("%d", *arr);
-
-     for(size_t i = 1; i < len; i++) {
-     	printf("%s%d", sep, arr[i]);
-     }
-
-     printf("\n"); 
-};
+    for(size_t i = 1; i < len; i++) {
+        printf("%s%d", sep, arr[i]);
+    }
+    printf("\n"); 
+}
 
 int main() {
-
     int arr[5] = {1,2,3,4,5};
+
     print_array(arr, ARRAY_SIZE(arr), NULL);
     apply(arr, ARRAY_SIZE(arr), cube);
+
     print_array(arr, ARRAY_SIZE(arr), NULL);
     apply_enumerate(arr, ARRAY_SIZE(arr), mult);
+    
     print_array(arr, ARRAY_SIZE(arr), NULL);
 
- return(0);
-};
+    return(0);
+}
